a1a9f6c593627ba3efdf22955a0082dcfe691570
dijaleo <mouloudidjalil@hotmail.fr>
2022-12-29 21:43:11 +0100

    Add class Personne body
    Completed the Personne class, might be subject to chenge, gitignore was
    randomly modified.

--- src/main/java/Logic/Personne.java
+++ src/main/java/Logic/Personne.java
@@ -5,9 +5,55 @@
 package Logic;
 
 /**
- *
+ * Uplabs
  * @author dijaleo
  */
-public class Personne {
+
+//imports et bibliothèques
+import java.util.Date; //pour la manipulation des dates de naissance
+
+
+public abstract class Personne {
     
+    //attributs
+    protected String nom, prenom;
+    protected Date dateDeNaissance; //le type peut changer
+    protected int ID;
+    protected static int i=0;
+    
+    //méthodes
+    public Personne(String nom, String prenom, Date dateDeNaissance) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.dateDeNaissance = dateDeNaissance;
+        this.ID=i++;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Date getDateDeNaissance() {
+        return dateDeNaissance;
+    }
+
+    public void setDateDeNaissance(Date dateDeNaissance) {
+        this.dateDeNaissance = dateDeNaissance;
+    }
+    
+    public int getID(){
+        return this.ID;
+    }
 }
