fd6f62704de7412677c7726159a3dc84bdb70a9e
dijaleo <mouloudidjalil@hotmail.fr>
2023-01-05 08:18:31 +0100

    [Aymen]Add Produit class

---
+++ src/main/java/Logic/Produit.java
@@ -0,0 +1,66 @@
+package Logic;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Tassili
+ */
+public class Produit {
+    
+    //attribut
+    private String nom;
+    private String description;
+    private String catégorie;
+    private int ref; //référence
+    private double prix;
+
+    //constructeur
+    public Produit(String nom, String description, String catégorie, int ref, double prix) {
+        this.nom = nom;
+        this.description = description;
+        this.catégorie = catégorie;
+        this.ref = ref;
+        this.prix = prix;
+    }
+    
+    //getter and setter
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCatégorie() {
+        return catégorie;
+    }
+
+    public void setCatégorie(String catégorie) {
+        this.catégorie = catégorie;
+    }
+
+    public int getRef() {
+        return ref;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
+
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
+}
